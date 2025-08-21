f120cac980096b2d459e505994c3b07e74a17fa0
mjykxz17 <mjykxz17@gmail.com>
2025-08-21 15:21:34 +0800

    Yoyo.java : rename class, add hello and goodbye

---
+++ src/main/java/Yoyo.java
@@ -0,0 +1,17 @@
+
+public class Yoyo {
+
+    public static void main(String[] args) {
+        String logo = """
+ ___                    __   __                
+|_ _|   __ _ _ __ ___   \\ \\ / /__  _   _  ___  
+ | |   / _` | '_ ` _ \\   \\ V / _ \\| | | |/ _ \\ 
+ | |  | (_| | | | | | |   | | (_) | |_| | (_) |
+|___|  \\__,_|_| |_| |_|   |_|\\___/ \\__, |\\___/ 
+                                   |___/       
+                """;
+        System.out.println("Hello from\n" + logo);
+        System.out.println("WHAT IS UP, my g");
+        System.out.println("Bye. Hope to see you again soon.");
+    }
+}
